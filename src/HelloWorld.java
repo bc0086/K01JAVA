328b004ce83700e200be687f96d9e3b848d4d705
bc0086 <bc0086@naver.com>
2020-08-24 23:58:54 +0900

    2020-08-24 23:58

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,7 +17,7 @@ public class HelloWorld {
 	public static void main(String[] args) { 
 		
 		System.out.println("Hello World");
-		System.out.println("처음 사용해보는 이클립스..!!");
+		System.out.println("처음 사용해보는 이클립스..!!!");
 	
 	}
 }
